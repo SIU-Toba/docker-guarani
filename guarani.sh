474cddf8974cad94656341ee0f8f1f8578b0a8f5
Andrés Blanco <ablanco@siu.edu.ar>
2015-05-12 15:45:15 -0300

    Se agrega variable de entorno para configurar el pass del usuario toba

--- guarani.sh
+++ guarani.sh
@@ -4,11 +4,16 @@ if [ -z "$TOBA_ID_DESARROLLADOR" ]; then
     TOBA_ID_DESARROLLADOR=0;
 fi
 
+if [ -z "$TOBA_PASS" ]; then
+    echo "Se utiliza el password default de toba (OjO)";
+    TOBA_PASS=toba;
+fi
+
 HOME_GESTION=/var/local/gestion
 
 if [ -z "$(ls -A "$HOME_GESTION/lib/toba/instalacion")" ]; then
     echo -n postgres > /tmp/clave_pg;
-    echo -n toba > /tmp/clave_toba;
+    echo -n ${TOBA_PASS} > /tmp/clave_toba;
     printf "\n" | ${HOME_GESTION}/lib/toba/bin/instalar -d ${TOBA_ID_DESARROLLADOR} -t 0 -h pg -p 5432 -u postgres -b toba_guarani -c /tmp/clave_pg -k /tmp/clave_toba;
     ${HOME_GESTION}/lib/toba/bin/toba proyecto cargar -d ${HOME_GESTION} -p guarani -i desarrollo;
     printf "\n" | ${HOME_GESTION}/bin/guarani instalar -j;
